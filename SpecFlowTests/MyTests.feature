e8fc34f136f8ebd14fb300d8ab4f3c25a1abf06f
Jay Zelos <jay@zelos.org.uk>
2019-04-20 19:34:55 +0100

    Added specflow test

---
+++ SpecFlowTests/MyTests.feature
@@ -0,0 +1,14 @@
+﻿Feature: MyTests
+
+# BDD test written in Gherkin using SpecFlow, Selenium and Web Driver. (and NUnit)
+# https://joebuschmann.com/getting-started-with-selenium-specflow-and-dot-net/
+
+# Install SpecFlow Visual Studio Extension to get template and generate feature file
+# Specify your tests within the feature file in Gherkin syntax and business language
+# Generate and populate your steps
+# Use Page Objects for clarity and to promote reuse.
+
+Scenario: Check the weather
+	Given I have a browser open with the BBC homepage displayed
+	When I click on the weather link in the title bar
+	Then the weather page is displayed
---
+++ SpecFlowTests/MyTestsSteps.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowTests
+{
+
+    [Binding]
+    public class MyTestsSteps
+    {
+        private readonly IWebDriver _webDriver;
+
+        public MyTestsSteps()
+        {
+            _webDriver = new ChromeDriver();
+        }
+
+        [Given(@"I have a browser open with the BBC homepage displayed")]
+        public void GivenIHaveABrowserOpenWithTheBBCHomepageDisplayed()
+        {
+            _webDriver.Navigate().GoToUrl("http://www.bbc.co.uk");
+        }
+
+        [When(@"I click on the weather link in the title bar")]
+        public void WhenIClickOnTheWeatherLinkInTheTitleBar()
+        {
+            IWebElement weatherLink = _webDriver.FindElement(By.CssSelector(".orb-nav-weather a"));
+            weatherLink.Click();
+        }
+
+        [Then(@"the weather page is displayed")]
+        public void ThenTheWeatherPageIsDisplayed()
+        {
+            // If this was dynamic HTML, we would create a web driver wait and 
+            // use wait.Until to poll until the found or a specified time expires.
+
+            Assert.AreEqual("https://www.bbc.co.uk/weather", _webDriver.Url);
+        }
+    }
+}
